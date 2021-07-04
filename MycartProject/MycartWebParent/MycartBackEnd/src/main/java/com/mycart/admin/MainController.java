4518e37165a4ed9b8d92c561f461912e4711b7fd
unknown <tiwari136@gmail.com>
2021-07-04 18:47:17 +0530

    java ecommerce project initial setup

---
+++ MycartProject/MycartWebParent/MycartBackEnd/src/main/java/com/mycart/admin/MainController.java
@@ -0,0 +1,21 @@
+package com.mycart.admin;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class MainController {
+	
+	
+	
+	@GetMapping(value = "/demo")
+	public ResponseEntity demo() {
+
+		return new ResponseEntity<>("Hi", HttpStatus.OK);
+	}
+
+}
---
+++ MycartProject/MycartWebParent/MycartBackEnd/src/main/java/com/mycart/admin/MycartBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mycart.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycartBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycartBackEndApplication.class, args);
+	}
+
+}
